dd599f2f6e10214f833c14a25e9ee21c1b489443
Duy247 <duy5a247@gmail.com>
2024-12-09 23:29:23 +0700

    new content

--- donators.php
+++ donators.php
@@ -3,21 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="website" property="og:type">
-    <meta content="Tài liệu chuyên lý Nguyễn Huệ" property="og:title">
-    <meta name="twitter:title" content="Tài liệu chuyên lý Nguyễn Huệ">
-    <meta property="og:description" content="Tổng hợp tài liệu chuyên lý ôn tập thi HSG các cấp và Olympics Vật lý.">
-    <meta property="og:site_name" content="PhysX-CNH">
-    <meta name="twitter:description" content="Tổng hợp tài liệu chuyên lý ôn tập thi HSG các cấp và Olympics Vật lý.">
-    <meta content="https://physx-cnh.com/image/logo_meta.png" property="og:image">
-    <meta property="og:image:width" content="1200" /> 
-    <meta property="og:image:height" content="630" /> 
-    <meta property="og:image:type" content="image/jpeg">
-    <meta content="https://physx-cnh.com/image/logo_meta.png" name="twitter:image">
-    <title>Tài liệu chuyên lý Nguyễn Huệ</title>
+
+    <title>Ủng hộ</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link rel="shortcut icon" type="image/x-icon" href="./image/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="../image/favicon.ico">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
         
@@ -161,6 +151,30 @@
             height: 100%; /* 100% of body height */
             width: 80%;
             float: left; /* Position the content next to the menu */
+        }
+        #content {
+            transition: filter 0.3s ease; /* Add a smooth transition for opacity */
+        }
+
+        #menu:hover ~ #content {
+            filter: brightness(0.2); 
+        }
+        .iframe-container {
+        flex: 1;
+        padding: 20px;
+        transition: filter 0.3s ease;
+        }
+        iframe {
+        width: 100%;
+        height: 100%;
+        border: none;
+        }
+        #top-part {
+            height: 60%;
+            padding-top:1rem;
+            box-sizing: border-box;
+            font-size: 1.0rem;     
+            text-align: justify;
             background-image: url('https://static.pexels.com/photos/414171/pexels-photo-414171.jpeg');
             background-size:cover;
                     -webkit-animation: slidein 100s;
@@ -174,21 +188,6 @@
 
                     -webkit-animation-direction: alternate;
                     animation-direction: alternate;
-        }
-        #content {
-            transition: filter 0.3s ease; /* Add a smooth transition for opacity */
-        }
-
-        #menu:hover ~ #content {
-            filter: brightness(0.2); 
-        }
-        #top-part {
-            height: 60%;
-            padding:1rem 2rem 0rem 2rem;
-            box-sizing: border-box;
-            font-size: 1.0rem;     
-            text-align: justify;
-            
         }
 
         @-webkit-keyframes slidein {
@@ -210,7 +209,7 @@
             margin: 0 10px;
             padding: 0.5em 1.0em 0.5em 1em;
             background: rgba(255, 255, 255, 0.6);
-            backdrop-filter: blur(5px);
+            backdrop-filter: blur(10px);
             border-radius: 10px;
             color: black;
             font-size: 1.8vh;
@@ -239,42 +238,26 @@
         #bottom-part {
             display: flex;
             clear: both;
-            background: rgba(255, 255, 255, 0.3);
-            backdrop-filter: blur(2px);
             height: 40%;
-            gap: 2%;
-            margin: 0rem 2rem 0rem 2rem;
-            border-radius:6px;
         }
 
         #l-bottom-part {
-            flex: 0 0 57%;          
+            flex: 0 0 60%;           
             box-sizing: border-box;
-            padding-top:0.5rem; 
-            padding-bottom: 0.5rem;
-            border-radius: 10px 10px 0 0;
-            margin-left:0.5rem;
         }     
-        #l-bottom-part img { /* Targets all images directly inside #r-bottom-part */
-            border-radius: 10px; /* Adjust the value as needed for the desired roundness */      
-        }
+
         #r-bottom-part {
             flex: 0 0 40%;
             box-sizing: border-box;
+            background-color: black;
             display: flex;
             align-items: center;
-            justify-content: center;           
-            border-radius: 10px 10px 0 0;
-            padding:0.5rem 0.5rem 0.5rem 0; 
-        }
-        #r-bottom-part img { /* Targets all images directly inside #r-bottom-part */
-            border-radius: 10px; /* Adjust the value as needed for the desired roundness */      
+            justify-content: center;
         }
         .slideshow-container {
+            max-width: 1000px;
             position: relative;
-            width: 100%;
-            height: 100%;
-            overflow: hidden;  
+            margin: auto;
         }
 
         .mySlides {
@@ -319,7 +302,7 @@
             position: relative;
             width: 100%;
             height: 100%;
-            overflow: hidden;           
+            overflow: hidden;            
         }
 
         .news-slide {
@@ -346,29 +329,24 @@
             background-color: rgba(0, 0, 0, 0.7);
             color: #fff;
             text-align: justify;
+            
         }
 
         .news-content h3 {
             padding-left:10px;
-            padding-right:2rem;
             margin: 0;
             font-size: 1.2vw;
             font-weight: bold;
         }
 
         .news-content p {
-            display:none;
             margin: 10px 0 0;
             font-size: 0.8vw;
+            opacity: 1;
             transition: opacity 0.3s ease-in-out;
             padding: 0px 40px 0px 10px;
         }
-        .news-content h3 ~ p {
-            display:none;
-        }
-        .news-content h3.show-paragraph ~ p {
-            display: block; /* Show the paragraph when the h3 has the class */
-        }
+
         .news-dots {
             position: relative;
             bottom: 20px;
@@ -450,32 +428,32 @@
 <body>
     <div id="menu">  
         <div id="m-top-part">
-            <img src="/image/logo.png" class="logo">
+            <img src="../image/logo.png" class="logo">
         </div>
         <div id="m-bottom-part">
             <ul class="menu-list">
-                <li><a href="./welcome">Trang chủ</a></li>
-                <li><a href="./nav/physics?type=magazines&level=all" id="materialsVltt">Tạp Chí</a></li>
-                <li><a href="./nav/physics?type=material&level=pho" id="materialsPreVpho">Tài Liệu</a></li>
+                <li><a href="../welcome">Trang chủ</a></li>
+                <li><a href="../nav/physics?type=magazines&level=all" id="materialsVltt">Tạp Chí</a></li>
+                <li><a href="../nav/physics?type=material&level=pho" id="materialsPreVpho">Tài Liệu</a></li>
                 <li class="dropdown">
                     <div class="dropdown-toggle">
                         <a>Sách</a>
                     </div>
                     <div class="dropdown-content">
-                        <a href="./nav/physics?type=book&level=pre-vpho" id="booksPreVpho">Sách Bồi Dưỡng<br>HSG Cấp Thành Phố</a>
-                        <a href="./nav/physics?type=book&level=vpho-vn" id="booksVn">Sách Tiếng Việt<br>Vòng 2 Thành Phố / HSGQG</a>
-                        <a href="./nav/physics?type=book&level=vpho-en" id="booksEn">Sách Tiếng Anh<br>Vòng 2 Thành Phố / HSGQG</a>
+                        <a href="../nav/physics?type=book&level=pre-vpho" id="booksPreVpho">Sách Bồi Dưỡng<br>HSG Cấp Thành Phố</a>
+                        <a href="../nav/physics?type=book&level=vpho-vn" id="booksVn">Sách Tiếng Việt<br>Vòng 2 Thành Phố / HSGQG</a>
+                        <a href="../nav/physics?type=book&level=vpho-en" id="booksEn">Sách Tiếng Anh<br>Vòng 2 Thành Phố / HSGQG</a>
                     </div>
                 </li>
-                <li><a href="./nav/physics?type=paper-sol&level=pho" id="materialsOlympiad">Đề Thi & Đáp Án</a></li>
-                <li><a href="./nav/physics?type=lessons&level=all" id="lessons">Nội Dung Ngày Học</a></li>
-                <li><a href="/daily/daily" id="dailyCurrent">Bài Tập Hàng Ngày</a></li>
-                <li><a href="./roadmap">Lộ trình ôn tập</a></li>
-                <li><a href="./donate" id="donate">Ủng hộ duy trì trang web</a></li>
+                <li><a href="../nav/physics?type=paper-sol&level=pho" id="materialsOlympiad">Đề Thi & Đáp Án</a></li>
+                <li><a href="../nav/physics?type=lessons&level=all" id="lessons">Nội Dung Ngày Học</a></li>
+                <li><a href="../daily/daily" id="dailyCurrent">Bài Tập Hàng Ngày</a></li>
+                <li><a href="../roadmap">Lộ trình ôn tập</a></li>
+                <li><a href="./donate">Ủng hộ duy trì trang web</a></li>
             </ul>
             <div class="bottom-links">
                 <ul class="menu-list-bottom">
-                    <li><a href="./disclaimer">Miễn Trừ Trách Nhiệm Pháp Lý</a></li>
+                    <li><a href="../disclaimer">Miễn Trừ Trách Nhiệm Pháp Lý</a></li>
                     <li><a href="welcome.php">Lượt Truy Cập: <span id="hitCount"> 0 </span></a></li>
                 </ul>
             </div>
@@ -483,93 +461,7 @@
     </div>
 
     <div id="content">
-        <div id="top-part">
-            <h1>Tài liệu chuyên lý Nguyễn Huệ</h1>
-            <div class="text-container">
-                <div class="text-box" id="introduce">
-                    <p>Đây là trang web tổng hợp tài liệu hỗ trợ học tập cho các học sinh thuộc đội tuyển vật lí. <br>Web được vận hành và duy trì trên nền tảng Github bởi Văn Thành Duy, chuyên lí Nguyễn Huệ K69.</p>
-                    <p>Trang web vận hành tốt nhất trên trình duyệt máy tính (PC / Chromebook / Samsung Dex). <br>Để truy cập vào các tài liệu, vui lòng chọn mục từ menu bên trái. </p>
-                    <p>Để yêu cầu tìm và bổ sung tài liệu, liên hệ <b><a href="mailto:duy5a247@gmail.com">duy5a247@gmail.com</a></b> hoặc tới <a href="https://github.com/Duy247/physx-cnh" style= "color:red;">repository github</a> và đọc thêm hướng dẫn <br>Nếu quá trình load và tải tài liệu bị chậm, hãy sử dụng DNS Cloudfare 1.1.1.1</p>         
-                    <p>Cảm ơn tới các cá nhân đã đóng góp tài liệu</p>
-                    <ul>
-                        <li>Văn Thành Duy Lý 1 K69 CNH | Phạm Quang Chính Lý 2 K75 CNH</li>
-                    </ul>
-                    <p>Cảm ơn tới các cá nhân đã đóng góp duy trì hosting</p>
-                    <ul>
-                        <li><a href="./donators" style="color: red;">Danh sách các bạn trẻ đã donate</a></li>
-                    </ul>
-                </div>
-                <div class="text-box" id="whats-new">
-                    <h2><span class="rainbow-text">Cập nhật mới</span></h2>
-                    <ul>                 
-                        <?php
-                        $whatsNewFile = './whats-new/whats_new.txt'; // Path to your text file
-                        if (file_exists($whatsNewFile)) {
-                            $lines = file($whatsNewFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-                            foreach ($lines as $line) {
-                                if (strpos($line, '* ') === 0) { // Check if the line starts with '* '
-                                    echo '<li>' . substr($line, 2) . '</li>'; // Remove '* ' and add to the list
-                                }
-                            }
-                        } else {
-                            echo '<li>No updates yet.</li>';
-                        }
-                        ?>
-                    </ul>
-                </div>
-            </div>
-        </div> 
-        <div id="bottom-part">
-            <div id="l-bottom-part">
-                <div class="news-slideshow-container">
-                    <?php
-                    $jsonFile = './whats-new/news.json';
-                    $jsonData = file_get_contents($jsonFile);
-                    $newsItems = json_decode($jsonData, true);
-
-                    foreach ($newsItems as $index => $newsItem) {
-                        $backgroundImage = $newsItem['background_image'];
-                        $headline = $newsItem['headline'];
-                        $detail = $newsItem['detail'];
-
-                        echo '<div class="news-slide">';
-                        echo '<img src="' . $backgroundImage . '" alt="News Image">';
-                        echo '<div class="news-content">';
-                        echo '<h3>' . $headline . '</h3>';
-                        echo '<p>' . $detail . '</p>';
-                        echo '</div>';
-                        echo '</div>';
-                    }
-                    ?>
-                </div>
-                <div class="news-dots">
-                    <?php
-                    $totalSlides = count($newsItems);
-                    for ($i = 0; $i < $totalSlides; $i++) {
-                        echo '<span class="news-dot" onclick="currentNewsSlide(' . ($i + 1) . ')"></span>';
-                    }
-                    ?>
-                </div>
-            </div>
-            <div id="r-bottom-part">
-                <div class="slideshow-container">
-                    <?php
-                    $imageDirectory = './physics/img/';
-                    $imageExtensions = ['jpg', 'jpeg', 'png', 'gif'];
-
-                    $images = glob($imageDirectory . '*.*');
-                    foreach ($images as $image) {
-                        $extension = pathinfo($image, PATHINFO_EXTENSION);
-                        if (in_array(strtolower($extension), $imageExtensions)) {
-                            echo '<div class="mySlides fade">';
-                            echo '<img src="' . $image . '" style="width:100%">';
-                            echo '</div>';
-                        }
-                    }
-                    ?>
-                </div>
-            </div>
-        </div>
+        <iframe id="content-iframe" src="./physics/donators.html" frameborder="0"></iframe>
     </div>
 
     <script>
@@ -583,22 +475,18 @@
         var materialsOlympiad = document.getElementById('materialsOlympiad');
         var materialsVltt = document.getElementById('materialsVltt');
         var lessons = document.getElementById('lessons');
-        var daily = document.getElementById('dailyCurrent');
-        var donate = document.getElementById('donate');
         
         if (isMobileDevice()) {         
-            booksPreVpho.href = "./nav/physics_mobile?type=book&level=pre-vpho";
-            booksVn.href = "./nav/physics_mobile?type=book&level=vpho-vn";
-            booksEn.href = "./nav/physics_mobile?type=book&level=vpho-en";
-            materialsPreVpho.href = "./nav/physics_mobile?type=material&level=pho";
-            materialsOlympiad.href = "./nav/physics_mobile?type=paper-sol&level=pho";
-            materialsVltt.href = "./nav/physics_mobile?type=magazines&level=all";
-            lessons.href = "./nav/physics_mobile?type=lessons&level=all";
-            daily.href = "./daily/current";
-            donate.href = "./physics/donate";
+            booksPreVpho.href = "../nav/physics_mobile?type=book&level=pre-vpho";
+            booksVn.href = "../nav/physics_mobile?type=book&level=vpho-vn";
+            booksEn.href = "../nav/physics_mobile?type=book&level=vpho-en";
+            materialsPreVpho.href = "../nav/physics_mobile?type=material&level=pho";
+            materialsOlympiad.href = "../nav/physics_mobile?type=paper-sol&level=pho";
+            materialsVltt.href = "../nav/physics_mobile?type=magazines&level=all";
+            lessons.href = "../nav/physics_mobile?type=lessons&level=all";
         }
         function updateHitCount() {
-            fetch('./visit_count/hit_counter.php') 
+            fetch('../visit_count/hit_counter.php') 
                 .then(response => response.json())
                 .then(data => {
                     const hitCount = data.count;
@@ -686,27 +574,7 @@
         newsSlideshow.addEventListener("mouseout", function() {
             newsSlideInterval = setInterval(function() {
                 plusNewsSlides(1);
-            }, 10000);
-        });
-
-        const headings = document.querySelectorAll('.news-content h3');
-
-        headings.forEach(heading => {
-        let timeoutId; // To store the timeout ID
-
-        heading.addEventListener('mouseenter', () => {
-            heading.classList.add('show-paragraph');
-
-            // Clear any existing timeout
-            clearTimeout(timeoutId); 
-        });
-
-        heading.addEventListener('mouseleave', () => {
-            // Set a timeout to remove the class after a delay
-            timeoutId = setTimeout(() => {
-            heading.classList.remove('show-paragraph');
-            }, 15000); // Adjust the delay (in milliseconds) as needed
-        });
+            }, 5000);
         });
     </script> 
 </body>
